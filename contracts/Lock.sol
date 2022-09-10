b86c8e5bcd0a407e7ca39820d7c67e1239e0e02d
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-09-10 04:41:31 -0300

    First commit 🚀

---
+++ contracts/Lock.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+contract Lock {
+    uint public unlockTime = 30000;
+    address payable public owner;
+
+    event Withdrawal(uint amount, uint when);
+
+    function withdraw() public {
+        // Uncomment this line to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+
+        emit Withdrawal(address(this).balance, block.timestamp);
+
+        owner.transfer(address(this).balance);
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '.env')
+})
+
+require("@nomicfoundation/hardhat-toolbox");
+require('./tasks/deploy')
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    local: {
+      protocol: 'http',
+      host: 'localhost',
+      port: 8545,
+      loggingEnabled: true,
+      chainId: 31337.,
+      url: 'http://127.0.0.1:8545'
+    },
+    mainnet: {
+      accounts: {
+        mnemonic: process.env.DEV_MNEMONIC
+      },
+      url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
+      network_id: 1
+    },
+    kovan: {
+      accounts: {
+        mnemonic: process.env.DEV_MNEMONIC
+      },
+      url: 'https://kovan.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
+      network_id: 42
+    },
+    goerli: {
+      accounts: {
+        mnemonic: process.env.DEV_MNEMONIC
+      },
+      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_PROJECT_ID,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_APIKEY,
+      kovan: process.env.ETHERSCAN_APIKEY,
+      goerli: process.env.ETHERSCAN_APIKEY
+    }
+  }
+};
